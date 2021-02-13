f02e8fc18e2aa01dd3924ffe7461a5ab5792a986
Nikolay Rademacher <hrrdx33@gmail.com>
2021-02-13 02:13:03 +0100

    reset

--- README.md
+++
@@ -1,3 +0,0 @@
-## My personal blog/homepage
-
-I'm writing posts about JavaScript on here, primarily to learn and explain concepts and features to myself.
--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
